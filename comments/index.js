c51e29e935fb451f48c146f60b9190c3cd9f347a
AntonioSamy <113845200+AntonioSamy@users.noreply.github.com>
2023-02-25 15:20:23 +0200

    create simple event bus

--- comments/index.js
+++ comments/index.js
@@ -1,19 +1,21 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const { randomBytes } = require("crypto")
+const cors = require("cors")
+const axios = require("axios")
 
 const app = express()
 
 let commentsByPostId = {};
 
-
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get("/posts/:id/comments", (req,res)=>{
     res.status(200).send(commentsByPostId[req.params.id] || [])
 })
 
-app.post("/posts/:id/comments", (req,res)=>{
+app.post("/posts/:id/comments", async (req,res)=>{
     const commentId = randomBytes(4).toString("hex");
     const {content} = req.body
     
@@ -21,7 +23,14 @@ app.post("/posts/:id/comments", (req,res)=>{
     
     comments.push({id:commentId, content})
     commentsByPostId[req.params.id] = comments
-    
+    await axios.post("http://localhost:4005/events",{
+        type:'CommentCreated',
+        data:{
+            id:commentId,
+            content,
+            postId:req.params.id
+        }
+    })
     res.status(201).send(comments)
 })
 
